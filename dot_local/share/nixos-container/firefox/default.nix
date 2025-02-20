5d2ae4daeb94cb13bab9d0f2698b62adbb0dfc72
PerfectionistCoder <github.hug438@outlook.com>
2025-02-20 11:41:23 +0000

    refactor nixos container

--- dot_local/share/nixos-container/firefox/default.nix
+++ dot_local/share/nixos-container/firefox/default.nix
@@ -15,6 +15,7 @@ let
     pathToImportAttr
     bindMountFile
     variables
+    mkContainer
     ;
 
   profileDir = "${variables.homeDir.local}/.mozilla/firefox";
@@ -27,60 +28,59 @@ in
   extraConfig,
   ...
 }:
-{
-  containers.${name} = mkMerge (
-    pathToImportAttr [
-      traits.user
-      traits.wayland
-      traits.pulseaudio
-      traits.cursor
+mkContainer {
+  inherit name args;
+  traits = [
+    "user"
+    "wayland"
+    "pulseaudio"
+    "cursor"
+  ];
+  config = {
+    bindMounts = {
+      profile = bindMountFile {
+        hostPath = variables.firefoxData;
+        mountPath = profileDir;
+        fileName = "profiles.ini";
+      };
+      userjs = bindMountFile {
+        hostPath = variables.firefoxData;
+        mountPath = "${profileDir}/default";
+        fileName = "user.js";
+      };
+      root = {
+        hostPath = "${variables.containerVolume}/${name}";
+        mountPoint = "${profileDir}/default";
+        isReadOnly = false;
+      };
+    };
+
+    config =
+      { pkgs, ... }:
       {
-        privateNetwork = true;
-
-        bindMounts = {
-          profile = bindMountFile {
-            hostPath = variables.firefoxData;
-            mountPath = profileDir;
-            fileName = "profiles.ini";
-          };
-          userjs = bindMountFile {
-            hostPath = variables.firefoxData;
-            mountPath = "${profileDir}/default";
-            fileName = "user.js";
-          };
-          root = {
-            hostPath = "${variables.containerVolume}/${name}";
-            mountPoint = "${profileDir}/default";
-            isReadOnly = false;
-          };
+        environment.sessionVariables = {
+          MOZ_ENABLE_WAYLAND = "1";
         };
-
-        config =
-          { pkgs, ... }:
-          {
-            environment.sessionVariables = {
-              MOZ_ENABLE_WAYLAND = "1";
+        programs.firefox = {
+          enable = true;
+          package = pkgs.firefox-esr;
+          policies = foldl (attr: name: attr // (import ./policies/${name}.nix)) {
+            Bookmarks = bookmarks;
+            Preferences = {
+              "browser.profile.name" = name;
             };
-            programs.firefox = {
-              enable = true;
-              package = pkgs.firefox-esr;
-              policies = foldl (attr: name: attr // (import ./policies/${name}.nix)) {
-                Bookmarks = bookmarks;
-                WebsiteFilter = mkIf visitBookmarksOnly {
-                  Block = [ "<all_urls>" ];
-                  Exceptions = map (
-                    bookmark:
-                    let
-                      url = bookmark.URL;
-                    in
-                    "https://" + (elemAt (splitString "/" (removePrefix "https://" url)) 0) + "/*"
-                  ) bookmarks;
-                };
-              } ([ "common" ] ++ policies);
+            WebsiteFilter = mkIf visitBookmarksOnly {
+              Block = [ "<all_urls>" ];
+              Exceptions = map (
+                bookmark:
+                let
+                  url = bookmark.URL;
+                in
+                "https://" + (elemAt (splitString "/" (removePrefix "https://" url)) 0) + "/*"
+              ) bookmarks;
             };
-          };
-      }
-      extraConfig
-    ] ({ inherit variables; } // args)
-  );
+          } ([ "common" ] ++ policies);
+        };
+      };
+  } // extraConfig;
 }
--- dot_local/share/nixos-container/utils/default.nix
+++ dot_local/share/nixos-container/utils/default.nix
@@ -1,18 +1,78 @@
 { lib, ... }:
 let
   inherit (builtins) typeOf readDir;
-  inherit (lib) mapAttrs' removeSuffix;
+  inherit (lib)
+    mapAttrs'
+    removeSuffix
+    mkMerge
+    mkDefault
+    ;
+  variables = import ./variables.nix;
 in
 {
-  pathToImportAttr =
-    list: args:
-    map (
-      x:
-      let
-        attr = if typeOf x == "path" then (import x) else x;
-      in
-      if typeOf attr == "lambda" then (attr args) else attr
-    ) list;
+  inherit variables;
+
+  mkContainer =
+    let
+      concatConfig =
+        list: args:
+        map (
+          x:
+          let
+            attr = if typeOf x == "path" then (import x) else x;
+          in
+          if typeOf attr == "lambda" then (attr ({ inherit variables; } // args)) else attr
+        ) list;
+      importTraits = mapAttrs' (name: _: {
+        name = removeSuffix ".nix" name;
+        value = import ./traits/${name};
+      }) (readDir ./traits);
+      minimalConfig = {
+        documentation = {
+          enable = mkDefault false;
+          doc.enable = mkDefault false;
+          info.enable = mkDefault false;
+          man.enable = mkDefault false;
+          nixos.enable = mkDefault false;
+        };
+        environment = {
+          defaultPackages = mkDefault [ ];
+          stub-ld.enable = mkDefault false;
+        };
+        programs = {
+          less.lessopen = mkDefault null;
+          command-not-found.enable = mkDefault false;
+        };
+        boot.enableContainers = mkDefault false;
+        services = {
+          logrotate.enable = mkDefault false;
+          udisks2.enable = mkDefault false;
+        };
+        xdg = {
+          autostart.enable = mkDefault false;
+          icons.enable = mkDefault false;
+          mime.enable = mkDefault false;
+          sounds.enable = mkDefault false;
+        };
+      };
+    in
+    {
+      name,
+      traits ? [ ],
+      config ? { },
+      args,
+    }:
+    {
+      containers.${name} = mkMerge (
+        concatConfig (
+          (map (trait: importTraits.${trait}) traits)
+          ++ [
+            config
+            { config = minimalConfig; }
+          ]
+        ) args
+      );
+    };
   bindMountFile =
     {
       hostPath,
@@ -23,9 +83,4 @@ in
       hostPath = "${hostPath}/${fileName}";
       mountPoint = "${mountPath}/${fileName}";
     };
-  traits = mapAttrs' (name: _: {
-    name = removeSuffix ".nix" name;
-    value = import ./traits/${name};
-  }) (readDir ./traits);
-  variables = import ./variables.nix;
 }
