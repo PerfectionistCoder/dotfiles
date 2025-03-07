7ec93c4fa220c62b62a81b97978302eeea3017c2
PerfectionistCoder <github.hug438@outlook.com>
2025-03-07 18:09:31 +0000

    more userjs config

--- dot_local/share/nixos-container/firefox/userjs/9_override.js
+++ dot_local/share/nixos-container/firefox/userjs/9_override.js
@@ -7,6 +7,17 @@ user_pref("font.size.monospace.x-western", 14);
 user_pref("font.size.variable.x-western", 20);
 
 // dns over https
-user_pref("network.trr.mode", 2);
+user_pref("network.trr.mode", 3);
 user_pref("network.trr.custom_uri", "https://dns.mullvad.net/dns-query");
 user_pref("network.trr.uri", "https://dns.mullvad.net/dns-query");
+
+// no session restore
+user_pref("browser.sessionstore.max_resumed_crashes", 0);
+
+// findbar highlight
+user_pref("findbar.highlightAll", true);
+
+// smooth scrolling
+user_pref("apz.overscroll.enabled", true);
+user_pref("general.smoothScroll", true);
+user_pref("mousewheel.default.delta_multiplier_y", 275);
