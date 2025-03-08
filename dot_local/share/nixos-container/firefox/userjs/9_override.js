9f7a5d3f6399659f1fa69e941c839623e98a8a43
PerfectionistCoder <github.hug438@outlook.com>
2025-03-08 15:43:00 +0000

    switch from arkenfox to pyllyukko userjs

--- dot_local/share/nixos-container/firefox/userjs/9_override.js
+++ dot_local/share/nixos-container/firefox/userjs/9_override.js
@@ -2,7 +2,7 @@ user_pref("font.default.x-western", "sans-serif");
 user_pref("font.name.monospace.x-western", "monospace");
 user_pref("font.name.sans-serif.x-western", "sans-serif");
 user_pref("font.name.serif.x-western", "serif");
-user_pref("font.size.monospace.x-western", 14);
+user_pref("font.size.monospace.x-western", 18);
 user_pref("font.size.variable.x-western", 20);
 
 user_pref("network.trr.mode", 3);
@@ -11,12 +11,14 @@ user_pref("network.trr.uri", "https://dns.mullvad.net/dns-query");
 
 user_pref("browser.sessionstore.max_resumed_crashes", 0);
 
-user_pref("findbar.highlightAll", true);
-
 user_pref("apz.overscroll.enabled", true);
 user_pref("general.smoothScroll", true);
 user_pref("mousewheel.default.delta_multiplier_y", 275);
 
-user_pref("extensions.pictureinpicture.enable_picture_in_picture_overrides", false);
+user_pref(
+  "extensions.pictureinpicture.enable_picture_in_picture_overrides",
+  false
+);
 
-user_pref("browser.urlbar.suggest.history", false)
+user_pref("keyword.enabled", true);
+user_pref("privacy.resistFingerprinting", false);
