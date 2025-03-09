52117420f25b785aab9f7d907c84fedd94145817
PerfectionistCoder <github.hug438@outlook.com>
2025-03-09 15:36:34 +0000

    update firefox userjs

--- dot_local/share/nixos-container/firefox/userjs/9_override.js
+++ dot_local/share/nixos-container/firefox/userjs/9_override.js
@@ -5,20 +5,25 @@ user_pref("font.name.serif.x-western", "serif");
 user_pref("font.size.monospace.x-western", 18);
 user_pref("font.size.variable.x-western", 20);
 
-user_pref("network.trr.mode", 3);
-user_pref("network.trr.custom_uri", "https://dns.mullvad.net/dns-query");
-user_pref("network.trr.uri", "https://dns.mullvad.net/dns-query");
-
 user_pref("browser.sessionstore.max_resumed_crashes", 0);
+
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+
+user_pref("browser.sessionstore.max_tabs_undo", 25);
+
+user_pref(
+  "browser.uiCustomization.state",
+  '{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":[],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","downloads-button","unified-extensions-button","fxa-toolbar-menu-button","reset-pbm-toolbar-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":[],"dirtyAreaCache":[],"currentVersion":20,"newElementCount":4}'
+);
 
 user_pref("apz.overscroll.enabled", true);
 user_pref("general.smoothScroll", true);
 user_pref("mousewheel.default.delta_multiplier_y", 275);
 
-user_pref(
-  "extensions.pictureinpicture.enable_picture_in_picture_overrides",
-  false
-);
+user_pref("network.trr.mode", 3);
+user_pref("network.trr.custom_uri", "https://dns.mullvad.net/dns-query");
+user_pref("network.trr.uri", "https://dns.mullvad.net/dns-query");
 
 user_pref("keyword.enabled", true);
 user_pref("privacy.resistFingerprinting", false);
